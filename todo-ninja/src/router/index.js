4e75555c95d01eadbca05195cd4c2f56d57647c2
nonna <ukr.nonna@gmail.com>
2020-08-14 11:45:20 -0400

    adding styles and data

---
+++ todo-ninja/src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Dashboard from '../views/Dashboard.vue'
+import Projects from '../views/Projects.vue'
+import Team from '../views/Team.vue'
+
+
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/team',
+    name: 'team',
+    component: Team
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: Projects
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ todo-ninja/src/views/Dashboard.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subtitle-1 grey--text">Dashboard</h1>
+    
+    <v-container class="my-5 py-1 grey lighten-4 rounded-0">
+      
+      <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn small text color="grey" @click="sortBy('title')" v-on="on">
+            <v-icon left small>mdi-folder</v-icon>
+            <span right class="caption text-lowercase">By project name</span>
+          </v-btn>
+        </template>
+        <span>Sort projects by project name</span>
+      </v-tooltip>
+
+      <v-tooltip top>
+        <template v-slot:activator="{ on }">
+          <v-btn small text color="grey" @click="sortBy('person')" v-on="on">
+            <v-icon left small>mdi-account</v-icon>
+            <span right class="caption text-lowercase">By person</span>
+          </v-btn>
+        </template>
+        <span>Sort projects by person</span>
+      </v-tooltip>
+
+      <v-card flat class="grey lighten-4" v-for="project in projects" :key="project.title">
+        <v-row row wrap :class="`rounded-0 pa-3 project ${project.status}`">
+          <v-col xs="12" md="6">
+            <div class="caption grey--text">Project Title</div>
+            <div>{{project.title}}</div>
+          </v-col>
+          <v-col xs="6" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>{{project.person}}</div>
+          </v-col>
+          <v-col xs="6" sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>{{project.due}}</div>
+          </v-col>
+          <v-col xs="2" sm="4" md="2">
+            <div class="text-right">
+              <v-chip small :class="`${project.status} white--text caption my-2`">{{ project.status }}</v-chip>
+            </div>
+          </v-col>
+        </v-row>
+        <v-divider></v-divider>
+      </v-card>
+
+    </v-container>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+        { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      ]
+    }
+  },
+  methods: {
+    sortBy(prop){
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+    }
+  }
+}
+</script>
+
+<style>
+
+ .v-card{
+    word-break: normal;
+  }
+
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+
+.project.ongoing{
+  border-left: 4px solid #e0c444;
+}
+
+.project.overdue{
+  border-left: 4px solid #ce1c16;
+}
+
+  .theme--light.v-chip.complete:not(.v-chip--active)  {
+    background: #3cd1c2;
+  }
+
+  .theme--light.v-chip.ongoing:not(.v-chip--active) {
+    background: #ffaa2c;
+  }
+
+  .theme--light.v-chip.overdue:not(.v-chip--active) {
+    background: #f83e70;
+  }
+</style>
